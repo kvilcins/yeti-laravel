138e7ea4f92149f9bc467236afd21d179b3e2e07
garina.sinergium.ru <garina@sinergium.ru>
2025-03-11 15:11:28 +0400

    Set up Laravel project on OpenServer 6 with PHP 8.3, MySQL 8.2, and Composer.

--- resources/views/components/partials/head.blade.php
+++ resources/views/components/partials/head.blade.php
@@ -2,11 +2,11 @@
     <meta charset="UTF-8">
     <title>@yield('title')</title>
     <!-- Подключение Normalize CSS -->
-    <link href="{{ asset('css/normalize.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/normalize.min.css') }}" rel="stylesheet">
     <!-- Подключение скомпилированных стилей -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
     <!-- Подключение скомпилированных JavaScript файлов -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ secure_asset('js/app.js') }}" defer></script>
     @isset($id)
         <meta name="lot-id" content="{{ $id }}">
     @endisset
