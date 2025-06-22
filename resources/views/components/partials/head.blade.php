47e711ccd3cfdc55aeba0fc21ce526dd71092ea5
garina@sinergium.ru <garina@sinergium.ru>
2025-06-22 15:03:11 +0400

    Adds favicons

--- resources/views/components/partials/head.blade.php
+++ resources/views/components/partials/head.blade.php
@@ -10,6 +10,10 @@
     <!-- Include compiled JavaScript files -->
     @vite('resources/js/app.js')
 
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicons/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicons/favicon-16x16.png') }}">
+
     @isset($lot)
         <meta name="lot-id" content="{{ $lot->id }}">
     @endisset
