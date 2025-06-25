d2e63b8f25b9b5f682b76f00bc461f237b235ef2
garina@sinergium.ru <garina@sinergium.ru>
2025-06-25 19:35:27 +0400

    Build production assets and deploy updates
    - Compiled CSS/JS for production
    - Updated webpack configuration
    - Fixed file upload symlinks
    - Configured email settings for production
    - Ready for production deployment

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+import path from 'path';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+    entry: {
+        app: ['./resources/js/app.js', './resources/css/app.scss']
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'js/[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css'
+        })
+    ]
+};
