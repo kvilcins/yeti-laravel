36efd8869307f23d380fdbc6df83ea8c6860f630
garina@sinergium.ru <garina@sinergium.ru>
2025-06-27 01:48:37 +0400

    Changes head from vite to webpack

--- resources/views/components/partials/head.blade.php
+++ resources/views/components/partials/head.blade.php
@@ -4,11 +4,13 @@
     <title>@yield('title')</title>
 
     <!-- Include Normalize CSS -->
-    @vite('resources/css/normalize.min.scss')
-    <!-- Include compiled styles -->
-    @vite('resources/css/app.scss') <!-- Include app.scss -->
-    <!-- Include compiled JavaScript files -->
-    @vite('resources/js/app.js')
+{{--    @vite('resources/css/normalize.min.scss')--}}
+{{--    <!-- Include compiled styles -->--}}
+{{--    @vite('resources/css/app.scss') <!-- Include app.scss -->--}}
+{{--    <!-- Include compiled JavaScript files -->--}}
+{{--    @vite('resources/js/app.js')--}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script type="module" src="{{ asset('js/app.js') }}"></script>
 
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicons/apple-touch-icon.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicons/favicon-32x32.png') }}">
